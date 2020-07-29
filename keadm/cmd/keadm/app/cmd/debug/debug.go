14ed8244acdeb3111173b84902a4b38031e504c7
qingchen1203 <qingchen_job@foxmail.com>
2020-07-29 11:47:31 +0800

    Add keadm debug command entry

---
+++ keadm/cmd/keadm/app/cmd/debug/debug.go
@@ -0,0 +1,36 @@
+/*
+Copyright 2020 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package debug
+
+import (
+	"github.com/spf13/cobra"
+	"io"
+)
+
+// NewEdgeDebug returns KubeEdge edge debug command.
+func NewEdgeDebug(out io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "debug",
+		Short: "Provide debug function to help diagnose the cluster",
+		Long:  "keadm debug command provide debug function to help diagnose the cluster",
+	}
+
+	// add subCommand
+	cmd.AddCommand(NewCmdDebugGet(out, nil))
+
+	return cmd
+}
